9a5226a3d7712c042f3cdae2f74923b33ec94536
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-06 16:28:28 -0700

    Task 1 : implemented basic menu items view with emojii placeholder for items

---
+++ LittleLemonDinnerMenu/LittleLemonDinnerMenu/MenuItemsView.swift
@@ -0,0 +1,55 @@
+//
+//  MenuItemsView.swift
+//  LittleLemonDinnerMenu
+//
+//  Created by Guy Steinberg on 06/04/2024.
+//
+
+import SwiftUI
+
+struct MenuItemsView: View {
+    let columns = [
+        GridItem(.flexible(minimum: 80), spacing: 10),
+        GridItem(.flexible(minimum: 80), spacing: 10),
+        GridItem(.flexible(minimum: 80), spacing: 10)
+    ]
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Menu")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(0x1f600...0x1f679, id: \.self) { value in
+                            GroupBox {
+                                Text(emoji(value))
+                                    .font(.largeTitle)
+                                    .fixedSize()
+                                Text(String(format: "%x", value))
+                                    .fixedSize()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .padding([.leading, .trailing], 20)
+        .toolbar {
+            ToolbarItem (){ Button(
+                action:{},
+                label: { Image(systemName: "slider.horizontal.3") })
+            }
+        }
+    }
+    
+    private func emoji(_ value: Int) -> String {
+        guard let scalar = UnicodeScalar(value) else { return "?" }
+        return String(Character(scalar))
+    }
+    
+}
+
+#Preview {
+    MenuItemsView()
+}
